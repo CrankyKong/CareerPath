347ef121990313a52b7fec5c8fe440e398d63b8d
Logan Skidmore <loganskidmore91@gmail.com>
2017-11-16 00:57:46 -0700

    Add project files.

---
+++ TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,17 @@
+﻿//simpleControls.js
+(function () {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            scope: {
+                displayWhen: "=displayWhen"
+            },
+            restrict: "E",
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+})();
